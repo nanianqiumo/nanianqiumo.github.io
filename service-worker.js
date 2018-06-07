bcaa76b85df8fe2b9df94d5d1781e9284f4255a7
LostInYou <nanianqiumo@foxmail.com>
2018-06-08 05:26:30 +0800

    init demmo

---
+++ service-worker.js
@@ -0,0 +1,77 @@
+importScripts('/static/js/workbox-sw.prod.v2.1.3.js');/**
+ * @file service-worker.js with workbox api
+ * @desc [example](https://workbox-samples.glitch.me/examples/workbox-sw/)
+ * @author YanDong(nanianqiumo@foxmail.com)
+ */
+
+/* globals WorkboxSW */
+
+const workboxSW = new WorkboxSW({
+    cacheId: 'lavas-cache',
+    ignoreUrlParametersMatching: [/^utm_/],
+    skipWaiting: true,
+    clientsClaim: true
+});
+
+// Define precache injection point.
+workboxSW.precache([
+  {
+    "url": "/index.html",
+    "revision": "f7d40177536a0e3fde0d48688daa0e7e"
+  },
+  {
+    "url": "/static/css/index.66ee3073.css"
+  },
+  {
+    "url": "/static/fonts/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "url": "/static/fonts/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "url": "/static/img/music-icon.8a348576.svg"
+  },
+  {
+    "url": "/static/js/index.1ddf179b.js"
+  },
+  {
+    "url": "/static/js/manifest.7253881e.js"
+  },
+  {
+    "url": "/static/js/vendor.bf4c5fd6.js"
+  },
+  {
+    "url": "/static/js/vue.cf6433b2.js"
+  },
+  {
+    "url": "/static/js/workbox-sw.prod.v2.1.3.js",
+    "revision": "a9890beda9e5f17e4c68f42324217941"
+  }
+]);
+workboxSW.router.registerNavigationRoute('/index.html');
+
+
+/**
+ * example runningCache with api
+ */
+// workboxSW.router.registerRoute(/^https:\/\/lavas\.baidu\.com\/some\/api/,
+//     workboxSW.strategies.networkFirst());
+
+
+/**
+ * example runningCache with resources from CDN
+ * including maxAge, maxEntries
+ * cacheableResponse is important for CDN
+ */
+// workboxSW.router.registerRoute(/^https:\/\/cdn\.baidu\.com/i,
+//     workboxSW.strategies.cacheFirst({
+//         cacheName: 'lavas-cache-images',
+//         cacheExpiration: {
+//             maxEntries: 100,
+//             maxAgeSeconds: 7 * 24 * 60 * 60
+//         },
+//         cacheableResponse: {
+//             statuses: [0, 200]
+//         }
+//     })
+// );
